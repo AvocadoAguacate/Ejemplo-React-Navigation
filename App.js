a3eb67b40b8077f54e1f05651f47523349223292
AvocadoAguacate <estebanguzra@gmail.com>
2021-07-06 01:52:22 -0500

    Tab y Stack navigation example

--- App.js
+++ App.js
@@ -1,21 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, SafeAreaView, Image, TouchableOpacity} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack'
 
-export default function App() {
+
+function CustomHeader({title}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flexDirection: 'row',height: 50}}>
+      <View style={{flex:1, justifyContent:'center'}}>
+        <Image style={{width:30, height:30, marginLeft: 5}} 
+        source={require('./src/images/lista.png')}
+        resizeMode="contain"/>
+      </View>
+      <View style={{flex:1.5, justifyContent: 'center'}}>
+        <Text style={{textAlign: 'center'}}>{title}</Text>
+      </View>
+      <View style={{flex:1}}></View>
     </View>
+  )
+}
+
+function HomeScreen({navigation}) {
+  return (
+    <SafeAreaView style={{ flex: 1}}>
+      <CustomHeader title="Home detail"/>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+        <TouchableOpacity 
+        style={{marginTop:20}}
+        onPress={() => navigation.navigate('HomeDetail')}
+        >
+          <Text>Go Home detail</Text>
+        </TouchableOpacity>
+      </View> 
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function HomeScreenDetail() {
+  return (
+    <SafeAreaView style={{ flex: 1}}>
+      <CustomHeader title="Home"/>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home detail!</Text>
+      </View> 
+    </SafeAreaView>
+  );
+}
+
+function SettingsScreen({navigation}) {
+  return (
+    <SafeAreaView style={{ flex: 1}}>
+      <CustomHeader title="Settings"/>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+        <TouchableOpacity 
+        style={{marginTop:20}}
+        onPress={() => navigation.navigate('SettingDetail')}
+        >
+          <Text>Go Setting detail</Text>
+        </TouchableOpacity>
+      </View> 
+    </SafeAreaView>
+  );
+}
+
+function SettingsScreenDetail() {
+  return (
+    <SafeAreaView style={{ flex: 1}}>
+      <CustomHeader title="Settings"/>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Setting detail!</Text>
+      </View> 
+    </SafeAreaView>
+  );
+}
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const navOptionHandler = () => ({
+  headerShown: false
+})
+
+function HomeStack () {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} options={navOptionHandler}/>
+      <Stack.Screen name="HomeDetail" component={HomeScreenDetail} options={navOptionHandler}/>
+    </Stack.Navigator>
+  )
+}
+
+function SettingStack () {
+  return (
+    <Stack.Navigator initialRouteName="Setting">
+      <Stack.Screen name="Setting" component={SettingsScreen} options={navOptionHandler}/>
+      <Stack.Screen name="SettingDetail" component={SettingsScreenDetail} options={navOptionHandler}/>
+    </Stack.Navigator>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Settings" component={SettingStack} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
